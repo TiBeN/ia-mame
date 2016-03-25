d314557cff181ea098ec39e49dabe0746ec23c48
Benjamin Legendre <legendre.benjamin@gmail.com>
2016-03-25 14:49:11 +0100

    Forgot to add this file before commit

---
+++ src/main/java/org/tibennetwork/iamame/internetarchive/collectionitem/MessChdPippin.java
@@ -0,0 +1,36 @@
+package org.tibennetwork.iamame.internetarchive.collectionitem;
+
+import java.io.File;
+import java.util.List;
+
+import org.tibennetwork.iamame.mame.Software;
+
+public class MessChdPippin extends SoftwareListCollectionItem {
+
+    private String softwareFileUrlPattern 
+        = "http://archive.org/download/MESS_0.149_CHD_pippin/" 
+            + "MESS_0.149_CHD_pippin.zip/MESS_0.149_CHD_pippin/%1$s/%2$s.chd";
+
+    public MessChdPippin (List<File> romsPaths, File writableRomPath) {
+        super(romsPaths, writableRomPath);
+    }
+
+    public void download (Software software) 
+            throws FileNotFoundInCollectionItem {
+        
+        String softwareName = software.getName();
+        String chdName = software.getChdName();
+        
+        String softwareFileUrl = String.format(
+            this.softwareFileUrlPattern,
+            softwareName,
+            chdName);
+
+        String destinationPath = this.writableRomPath.getAbsolutePath()
+            + File.separator
+            + software.getRelativeFilePath();
+
+        this.downloadFile(softwareFileUrl, destinationPath);
+
+    }
+}
