dfcd35f92df0aa248ab41dda29a9e206c4a9d185
Benjamin Legendre <legendre.benjamin@gmail.com>
2016-04-13 13:43:14 +0200

    Configured project source tree to host Tests and added a Sandbox class to test things

---
+++ src/test/java/org/tibennetwork/iamame/TestTrash.java
@@ -0,0 +1,17 @@
+package org.tibennetwork.iamame;
+
+import org.junit.Test;
+
+/**
+ * Sandbox to tests things.
+ */
+public class TestTrash {
+
+    @Test
+    public void testSomething() throws Exception {
+
+        // Insert code to test here
+
+    }
+        
+}
