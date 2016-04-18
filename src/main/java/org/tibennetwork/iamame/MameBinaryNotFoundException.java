babb8b9b1720a3008805301ee7002e366081a4b0
Benjamin Legendre <legendre.benjamin@gmail.com>
2016-04-18 15:37:06 +0200

    Ooups, forgot to add this exception class on the previous commit

---
+++ src/main/java/org/tibennetwork/iamame/MameBinaryNotFoundException.java
@@ -0,0 +1,11 @@
+package org.tibennetwork.iamame;
+
+public class MameBinaryNotFoundException extends Exception {
+
+    static final long serialVersionUID = 142L;
+
+    public MameBinaryNotFoundException (String s) {
+        super(s);
+    }
+
+}
