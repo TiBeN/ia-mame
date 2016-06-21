222c92cc4e3cc40a5a7d9fd61f204c549f20afba
Benjamin Legendre <legendre.benjamin@gmail.com>
2016-06-21 13:59:15 +0200

    Tests WIP

--- src/test/java/org/tibennetwork/iamame/mame/TestMachine.java
+++ src/test/java/org/tibennetwork/iamame/mame/TestMachine.java
@@ -1,5 +1,8 @@
 package org.tibennetwork.iamame.mame;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,15 +12,29 @@ import org.junit.Test;
 public class TestMachine {
 
     @Test
-    public void testGetNeededRomsFilesWithMachineNeedingOnlyOneFile () {
+    public void testGetNeededRomsFilesWithMachineNeedingOnlyOneFile ()
+            throws FileNotFoundException,
+                IOException,
+                InterruptedException,
+                MachineDoesntExistException {
         
         FakeMameRuntime mame = new FakeMameRuntime();
         
         List<InputStream> inputStreams = new ArrayList<>();
+
+        inputStreams.add(
+                new FileInputStream("src/test/resources/xml/altbeast.xml"));
         
         mame.setInputStreamsToReturn(inputStreams);
 
         MachineRepository mr = new MachineRepository(mame);
+
+        Machine m = mr.findByName("altbeast");
+
+        List<String> neededFiles = m.getNeededRomFiles();
+
+        // Replace return argument type of methods 
+        // get{Needed|Missing}{Rom|Chd}Files from List to Set
 
     }
 
