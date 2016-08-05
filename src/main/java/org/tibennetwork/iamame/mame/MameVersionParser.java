526b51575ad68b60e269bf17592049599aaf0cd9
Benjamin Legendre <legendre.benjamin@gmail.com>
2016-08-05 15:02:08 +0200

    Added MameVersionParser class skeleton + test

---
+++ src/main/java/org/tibennetwork/iamame/mame/MameVersionParser.java
@@ -0,0 +1,13 @@
+package org.tibennetwork.iamame.mame;
+
+public class MameVersionParser {
+
+    /**
+     * Parse and return the version of Mame from the first line
+     * of the output of the -help command
+     */
+    public String parse (String helpCommandFirstLine) {
+        return null;
+    }
+
+}
---
+++ src/test/java/org/tibennetwork/iamame/mame/TestMameVersionParser.java
@@ -0,0 +1,5 @@
+package org.tibennetwork.iamame.mame;
+
+public class TestMameVersionParser {
+
+}
