7ed809724bd2aa2500f912c86b2dd4c5c13b0dd0
Benjamin Legendre <legendre.benjamin@gmail.com>
2016-08-16 13:32:36 +0200

    Written tests (disabled for now) for MameVersionParser class

---
+++ src/main/java/org/tibennetwork/iamame/mame/UnhandledMameVersionPatternException.java
@@ -0,0 +1,11 @@
+package org.tibennetwork.iamame.mame;
+
+public class UnhandledMameVersionPatternException extends Exception {
+
+    static final long serialVersionUID = 4L;
+
+    public UnhandledMameVersionPatternException (String s) {
+        super(s);  
+    }
+
+}
--- src/test/java/org/tibennetwork/iamame/mame/TestMameVersionParser.java
+++ src/test/java/org/tibennetwork/iamame/mame/TestMameVersionParser.java
@@ -1,5 +1,47 @@
 package org.tibennetwork.iamame.mame;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class TestMameVersionParser {
 
+    //@Test
+    public void testParsingMameVersion0162 () {
+
+        String versionLine = "M.A.M.E. v0.162 (Jul 19 2016) - Multiple Arcade Machine Emulator";
+        MameVersionParser mvp = new MameVersionParser();
+
+        assertThat(mvp.parse(versionLine), equalTo("0.162"));
+
+    }    
+
+    //@Test
+    public void testParsingMameVersion0170 () {
+
+        String versionLine = "MAME v0.170 (Jun 16 2016)";
+        MameVersionParser mvp = new MameVersionParser();
+
+        assertThat(mvp.parse(versionLine), equalTo("0.170"));
+
+    }
+
+    //@Test
+    public void testParsingMameVersion0175 () {
+
+        String versionLine = "MAME v0.175 (mame0175)";
+        MameVersionParser mvp = new MameVersionParser();
+
+        assertThat(mvp.parse(versionLine), equalTo("0.175"));
+
+    }
+
+    //@Test(expected = UnhandledMameVersionPatternException.class)
+    public void testParsingMameWithUnhandledMameVersionPattern () {
+
+        String versionLine = "Mame unhandled pattern 0.xx";
+        MameVersionParser mvp = new MameVersionParser();
+
+        mvp.parse(versionLine);
+
+    }
 }
